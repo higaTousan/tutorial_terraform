9071a20bbc95a4134cfdd992c7af3e533bb7eb67
toichiro <toichiro@cloudpack.jp>
2020-06-03 14:26:23 +0900

    Resource usage explanation

---
+++ vpc.tf
@@ -0,0 +1,9 @@
+resource "aws_vpc" "test-vpc" {
+  cidr_block           = "10.0.0.0/16"
+  instance_tenancy     = "default"
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "test-vpc"
+  }
+}
