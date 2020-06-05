4217b4a312ec14f720a04c38d54abe3b8e42aa48
toichiro <toichiro@cloudpack.jp>
2020-06-05 22:28:11 +0900

    Added explanation of how to use terraform-aws-modules

--- ec2.tf
+++ ec2.tf
@@ -20,8 +20,11 @@ data "aws_ami" "amazon_linux2" {
   }
 }
 
-resource "aws_instance" "example" {
-  ami           = data.aws_ami.amazon_linux2.image_id
-  instance_type = "t3.micro"
-  subnet_id     = aws_subnet.test-subnet.id
+module "ec2" {
+  source = "terraform-aws-modules/ec2-instance/aws"
+  instance_count              = 1
+  name                        = "test-ec2"
+  ami                         = data.aws_ami.amazon_linux2.image_id
+  instance_type               = "t3.micro"
+  subnet_id                   = aws_subnet.test-subnet.id
 }
